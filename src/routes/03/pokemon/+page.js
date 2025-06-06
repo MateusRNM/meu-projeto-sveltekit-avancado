fa30b16ea15b8485f3831bf8f80af378670edad3
MateusRNM <mateusmt676@gmail.com>
2025-06-06 08:31:11 -0400

    update

--- src/routes/03/pokemon/+page.js
+++ src/routes/03/pokemon/+page.js
@@ -1,15 +1,18 @@
-export async function load({url}) {
-    const limit = url.searchParams.get('limit') || 12;
-    const page = url.searchParams.get('page') || 1;
-    const offset = (page-1) * 12
+export async function load({url, fetch}) {
+    let limit = url.searchParams.get('limit') || 12;
+    let page = url.searchParams.get('page') || 1;
+    let name = url.searchParams.get('name') || "";
+    let tipo = url.searchParams.get('tipo') || 0;
+    let offset = (page-1) * limit
 
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-    const data = await res.json();
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${name == "" ? offset : 0}&limit=${name == "" ? limit : 2000}`);
+    let data = await res.json();
+    data.results = data.results.filter((d) => d.name.includes(name))
 
     for (const pokemon of data.results) {
         const id = pokemon.url.split("/").at(-2);
         pokemon.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     }
 
-    return { pokemons: data.results };
+    return { pokemons: data.results, limitPast: limit, pagePast: page, namePast: name, typeLast: tipo };
 }
--- src/routes/03/pokemon/+page.svelte
+++ src/routes/03/pokemon/+page.svelte
@@ -1,47 +1,87 @@
 <script>
-    import { derived } from 'svelte/store';
-
+  import { types } from '$lib/data.js';
   let { data } = $props();
-  let page = $state(1)
-  let limit = $state(12);
+  let page = $state(data.pagePast);
+  let limit = $state(data.limitPast);
+  let name = $state(data.namePast);
+  let filtrarTipo = $state(data.typeLast);
 
-  function anterior(){
-    if(page <= 1) return
-    page -= 1
+  function anterior() {
+    if (page <= 1) return;
+    page -= 1;
   }
 
-  function proximo(){
-    page += 1
+  function proximo() {
+    page += 1;
   }
 </script>
 
-<div class="row">
-  {#each data.pokemons as p}
-    <div class="col-md-3 col-sm-6 mb-3">
-      <div class="card">
-        <img src={p.image} alt={p.name} class="card-img-top" />
-        <div class="card-body">
-          <a href="/03/pokemon/{p.name}" class="stretched-link text-decoration-none">
-            <h5 class="card-title text-capitalize">{p.name}</h5>
-          </a>
+<form action="/03/pokemon">
+  <div class="row mt-2 mb-5">
+    <input
+      type="text"
+      class="form-control w-50 position-absolute start-50 translate-middle-x"
+      placeholder="Digite o nome do pokémon"
+      name="name"
+      bind:value={name}
+    />
+  </div>
+  <div class="row mt-2 mb-3">
+    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+      <input
+      style="visibility: hidden; position: absolute;"
+      class="text-center border border-0"
+      readonly
+      name="tipo"
+      bind:value={filtrarTipo}
+      />
+      {#each types as type}
+        <input type="radio" class="btn-check" autocomplete="off" checked={filtrarTipo == type.value} onclick={() => filtrarTipo = type.value}/>
+        <label class="btn btn-outline-primary" for="btnradio1">{type.name}</label>
+      {/each}
+    </div>
+  </div>
+
+  <div class="container mb-5">
+    <button class='btn btn-primary w-50 text-center position-absolute start-50 translate-middle-x'>Filtrar</button>
+  </div>
+
+  <div class="row">
+    {#each data.pokemons as p}
+      <div class="col-md-3 col-sm-6 mb-3 mt-5">
+        <div class="card">
+          <img src={p.image} alt={p.name} class="card-img-top" />
+          <div class="card-body">
+            <a href="/03/pokemon/{p.name}" class="stretched-link text-decoration-none">
+              <h5 class="card-title text-capitalize">{p.name}</h5>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  {/each}
-</div>
+    {/each}
+  </div>
 
-<form action={`/03/pokemon`}>
   <div class="container text-center">
     <button class="btn btn-primary m-3" onclick={() => anterior()}>ANTERIOR</button>
-    <input class="w-5 text-center border border-0" disabled={true} name="page" bind:value={page}/>
+    <input class="w-5 text-center border border-0" readonly name="page" bind:value={page} />
     <button class="btn btn-primary m-3" onclick={() => proximo()}>PRÓXIMO</button>
-    <p>Quantidade de pokémons por página:</p> <input class="text-center border border-0" name="limit" disabled={true} bind:value={limit}/>
+    <p>Quantidade de pokémons por página:</p>
+    <input
+      style="visibility: hidden; position: absolute;"
+      class="text-center border border-0"
+      readonly
+      name="limit"
+      bind:value={limit}
+    />
+    <button class={limit == 12 ? 'btn btn-primary m-1' : 'btn btn-outline-primary'} onclick={() => (limit = 12)}>12</button>
+    <button class={limit == 24 ? 'btn btn-primary m-1' : 'btn btn-outline-primary'} onclick={() => (limit = 24)}>24</button>
+    <button class={limit == 36 ? 'btn btn-primary m-1' : 'btn btn-outline-primary'} onclick={() => (limit = 36)}>36</button>
   </div>
 </form>
-
 
 <style>
   input {
     width: 6%;
+    outline: 0;
   }
 </style>
